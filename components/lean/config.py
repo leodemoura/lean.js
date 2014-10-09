0b5f0076bd17f36590034a5e6170e0aefe48a2fc
Soonho Kong <soonhok@cs.cmu.edu>
2014-10-09 12:56:03 -0700

    feat(components): add lean

---
+++ components/lean/config.py
@@ -0,0 +1,28 @@
+NAME = 'lean'
+VERSION = '0.2.0'
+DOWNLOADS = ['https://github.com/soonhokong/lean/archive/master.zip']
+SOURCE_DIR = 'lean-master'
+CONFIGURE_CMD = ' '.join([
+    'cmake',
+    '-DGMP_INCLUDE_DIR:STRING={includes}/gmp',
+    '-DGMP_LIBRARIES:STRING={libs}/libgmp.so',
+    '-DMPFR_INCLUDES:STRING={includes}/mpfr',
+    '-DMPFR_LIBRARIES:STRING={libs}/libmpfr.so',
+    '-DLUA_INCLUDE_DIR:STRING={includes}/lua',
+    '-DLUA_LIBRARIES:STRING={libs}/liblua.so',
+    '-DCMAKE_TOOLCHAIN_FILE={component_dir}/cmake-emcc-toolchain.txt',
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DTCMALLOC=OFF',
+    '-DCMAKE_CXX_FLAGS="-v -U__SSE2_MATH__ --ignore-dynamic-linking -U__GNUG__"',
+    'src/'
+])
+MAKE_CMD = 'emmake make -j'
+ARTIFACTS =  {
+    'includes': [
+        {'source':'CGAL-%s/include/' % VERSION, 'name':'cgal'}
+    ],
+    'libs': [
+        {'source': 'CGAL-%s/lib/libCGAL.so' % VERSION,'name':'libcgal.so'},
+        {'source': 'CGAL-%s/lib/libCGAL_Core.so' % VERSION,'name':'libcgal_core.so'},
+    ]
+}
