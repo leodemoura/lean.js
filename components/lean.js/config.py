adf00f30777f82c21bdb27251d75ea7013e9786a
Soonho Kong <soonhok@cs.cmu.edu>
2014-10-20 19:35:54 -0400

    feat(component): add lean.js and lean

--- components/lean.js/config.py
+++ components/lean.js/config.py
@@ -4,9 +4,18 @@ DOWNLOADS = ['https://github.com/leanprover/lean/archive/master.tar.gz']
 SOURCE_DIR = 'lean-master'
 CONFIGURE_CMD = ' '.join([
     'cmake',
+    '-DGMP_INCLUDE_DIR:STRING={includes}/gmp',
+    '-DGMP_LIBRARIES:STRING={libs}/libgmp.so',
+    '-DMPFR_INCLUDES:STRING={includes}/mpfr',
+    '-DMPFR_LIBRARIES:STRING={libs}/libmpfr.so',
+    '-DLUA_INCLUDE_DIR:STRING={includes}/lua',
+    '-DLUA_LIBRARIES:STRING={libs}/liblua.so',
+    '-DCMAKE_TOOLCHAIN_FILE={component_dir}/cmake-emcc-toolchain.txt',
     '-DCMAKE_BUILD_TYPE=Release',
     '-DTCMALLOC=OFF',
+    '-DMULTI_THREAD=OFF',
     '-DIGNORE_SORRY=ON',
+    '-DCMAKE_CXX_FLAGS="-v -U__SSE2_MATH__ --ignore-dynamic-linking -U__GNUG__"',
     'src/'
 ])
 MAKE_CMD = 'emmake make -j'
